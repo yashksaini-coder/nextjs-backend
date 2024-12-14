315e5821a437b3a2a3f2137e2decc88c40d5a467
yashksaini-coder <ys3853428@gmail.com>
2024-12-14 23:37:24 +0530

    feat: 🎸 Add Prisma client & POST endpoint

---
+++ app/api/v1/signup/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest, NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const client = new PrismaClient();
+
+export async function POST(req: NextRequest) {
+    const body = await req.json();
+    // should add zod validation here
+    const user = await client.user.create({
+        data: {
+            username: body.username,
+            password: body.password
+        }
+    });
+
+    console.log(user.id);
+
+    return NextResponse.json({ message: "Signed up" });
+}
