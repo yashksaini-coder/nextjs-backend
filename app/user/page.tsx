a3c49c3f34522d7cb9e2799caf7369ff3e04e869
yashksaini-coder <ys3853428@gmail.com>
2024-12-14 21:03:17 +0530

    ci: 🎡 Add loader component and add it to rendering in user page

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -1,9 +1,10 @@
 import axios from "axios"
-
+import Loading from "./loading";
 export default async function User(){
     const api = "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
     
     const response = await axios.get(api);
+    await new Promise(r=> setTimeout(r, 5000));
     const data = response.data;
     console.log(data.data)
 
@@ -11,5 +12,5 @@ export default async function User(){
         <div>
             {data.name}
             {data.email}
-    </div>
+        </div>
 )}
