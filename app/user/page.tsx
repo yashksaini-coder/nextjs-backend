02cde0bcc4cabebea3957a4441e6532bbbed8986
yashksaini-coder <ys3853428@gmail.com>
2024-12-14 20:50:37 +0530

    test: 💍 Async data fetching in server side

--- app/user/page.tsx
+++ app/user/page.tsx
@@ -4,30 +4,16 @@ import { useEffect } from "react"
 import axios from "axios"
 import { useState } from "react"
 
-export default function User(){
-    const [loading, setLoading] = useState(true)
-    const [data, setData] = useState(null)
+export default async function User(){
     const api = "https://week-13-offline.kirattechnologies.workers.dev/api/v1/user/details"
-    useEffect(() => {
-        if (api) {
-            axios.get(api)
-            .then((response) => {
-                setData(response.data)
-                setLoading(false)
-            })
-        } else {
-            console.error("API URL is not defined");
-            setLoading(false);
-        }
-    }, [])
-
-    if(loading){
-        return <h1>Loading...</h1>
-    }
     
+    const response = await axios.get(api);
+    const data = response.data;
+    console.log(data.data)
+
     return(
         <div>
             {data.name}
             {data.email}
     </div>
-)}   
+)}
